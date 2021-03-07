62a0cac5751527308949c1075c497b952c6b7f3a
frederic <fredericwalter.lehr@gmail.com>
2021-03-07 04:05:55 +0100

    use telegram instead of aws as not. client; fix bug in index.js

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-import SNS from 'aws-sdk/clients/sns.js';
 import getBinanceData from './utils/klines.js';
 import computeRsi from './utils/compute.js';
 import config from './config.js';
 import sqlite3 from 'sqlite3';
+import axios from 'axios';
 
-const THRESHOLD = 42.0
+const THRESHOLD = 50.0
 
 const INTERVAL = '4h'
 
 const db = new sqlite3.Database(config.db.name)
-
-const sns = new SNS({
-    endpoint: `https://sns.${config.aws.region}.amazonaws.com`,
-    accessKeyId: config.aws.accessKeyId,
-    secretAccessKey: config.aws.secretAccessKey,
-    region: config.aws.region,
-    sslEnabled: true
-});
 
 const binanceData = await getBinanceData(INTERVAL)
 
@@ -32,18 +24,18 @@ for (let symbol in rsiValues){
                 
                 db.run(`UPDATE ${config.db.table} SET isBelowThreshold = 1 WHERE symbols="${symbol}";`);
 
-                const alert = {
-                    Subject: 'RSI Alert',
-                    Message: `${new Date()} Binance Alert [${symbol}USDT/${INTERVAL}]: RSI unter ${THRESHOLD}`,
-                    TopicArn: `arn:aws:sns:${config.aws.region}:${config.aws.userId}:binanceAlerts`
-                };
-        
-                sns.publish(alert, (e, res) => {
-                    if (e) console.log(e, e.stack);
-                    else console.log(`${new Date()} Sending alert for ${symbol} >> MessageId: ${res.MessageId}`);
+                axios.post(config.telegram.baseUrl + config.telegram.accessToken + '/sendMessage', {
+                    chat_id: config.telegram.groupId,
+                    text: `${new Date()} Binance Alert [${symbol}USDT/${INTERVAL}]: RSI unter ${THRESHOLD}`
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
                 });
 
-            } else {
+            } else if (rsiValues[symbol] >= THRESHOLD) {
 
                 db.run(`UPDATE ${config.db.table} SET isBelowThreshold = 0 WHERE symbols="${symbol}";`);
             }
